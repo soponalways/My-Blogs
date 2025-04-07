3a9f3396db1c2076cba959890af6a4df41b307c1
soponalways <soponalways@gmail.com>
2025-04-07 23:55:31 +0600

    Add some code

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,11 @@ import Blogs from './components/Blogs/Blogs';
 const App = () => {
   const [bookmarked, setBookmarked] = useState([]);
   const handleBookmarked = (blog) => {
+    // console.log(blog);
     setBookmarked([...bookmarked, blog]);
+    // const bookmarkedBlogs = bookmarked.filter(bookmarkedBlog => bookmarkedBlog.id !== blog.id)
+    // console.log("This is Bookmarked Data ", bookmarkedBlogs);
+    // setBookmarked(bookmarkedBlogs)
   }
   return (
     <div>
@@ -14,9 +18,15 @@ const App = () => {
         <div className="left-container w-[65%]">
           <Blogs handleBookmarked={handleBookmarked}></Blogs>
         </div>
-        <div className="right-container w-[30%]">
-          <h1 className='text-2xl font-semibold my-3'>Reading Time: 0</h1>
-          <h1 className='text-2xl font-semibold my-3'>Bookmarked Count : {bookmarked.length}</h1>
+        <div className="right-container w-[35%]">
+          <div>
+            <h1 className='text-2xl font-semibold my-3'>Reading Time: 0</h1>
+            <h1 className='text-2xl font-semibold my-3'>Bookmarked Count : {bookmarked.length}</h1>
+          </div>
+          <div>
+            {/* Here are Show all The Book bookmarked */}
+
+          </div>
         </div>
       </div>
     </div>
---
+++ src/components/Bookmarked/Bookmarked.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Bookmarked = () => {
+    return (
+        <div>
+            Hello from book marked
+        </div>
+    );
+};
+
+export default Bookmarked;
